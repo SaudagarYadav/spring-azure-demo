65cab71fe9f61c6b2d23c1e2a2e18959e34f3ca6
Saudagar Yadav <saudyadav@deloitte.com>
2021-07-03 23:05:05 +0530

    Test

--- src/main/java/com/javatechie/azure/demo/Test.java
+++ src/main/java/com/javatechie/azure/demo/Test.java
@@ -3,7 +3,7 @@ package com.javatechie.azure.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController(value = "")
 public class Test {
 
 	@GetMapping("/deploy")
