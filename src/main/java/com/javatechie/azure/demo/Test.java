1fac19dcbda43764298f72905c236442a4db3136
Saudagar Yadav <saudyadav@deloitte.com>
2021-07-03 22:57:50 +0530

    Add get controller

--- src/main/java/com/javatechie/azure/demo/Test.java
+++ src/main/java/com/javatechie/azure/demo/Test.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test {
 
-	@GetMapping("Deploy")
+	@GetMapping("/deploy")
 	public String test() {
 		return "Finally deployed";
 	}
