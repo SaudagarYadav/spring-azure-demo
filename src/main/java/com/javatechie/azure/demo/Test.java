45b42b1be854bf2e445cd0f2b169fec3763e18bd
Saudagar Yadav <saudyadav@deloitte.com>
2021-07-04 18:34:55 +0530

    App configuration

--- src/main/java/com/javatechie/azure/demo/Test.java
+++ src/main/java/com/javatechie/azure/demo/Test.java
@@ -3,8 +3,15 @@ package com.javatechie.azure.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.azure.data.appconfiguration.ConfigurationClient;
+import com.azure.data.appconfiguration.ConfigurationClientBuilder;
+import com.azure.data.appconfiguration.models.ConfigurationSetting;
+
+
 @RestController()
 public class Test {
+
+	private ConfigurationClient configurationClient;
 
 	@GetMapping("/deploy")
 	public String test() {
@@ -13,7 +20,12 @@ public class Test {
 	
 	@GetMapping("/map")
 	public String test1() {
-		return "Another mapping working";
+		
+		configurationClient = new ConfigurationClientBuilder()
+			    .connectionString("Endpoint=https://testappfeature.azconfig.io;Id=rf3B-l0-s0:WwOhYCrWi9tVxih6c4+A;Secret=OjTFPEt+54FgTTv8oLk2eJ+qCn6Z/Gb9Tgx2Fhd62Dw=")
+			    .buildClient();
+		return configurationClient.getConfigurationSetting("certification", "").getValue();
+		
 	}
 	
 	
